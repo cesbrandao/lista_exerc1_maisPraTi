4b9fd7c407e1f6021a83aff0baa12d1e7c986230
cesbrandao <ceduardosbrandao@gmail.com>
2025-05-01 00:24:44 -0300

    feat: melhoria na validação da classificação dos lados do triângulo

--- exercicio06.js
+++ exercicio06.js
@@ -1,5 +1,12 @@
 /**
- * descricao
+ * Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos
+ * formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo:
+ * Isósceles, escaleno ou eqüilátero.
+ * 
+ * Para verificar se os lados fornecidos formam triângulo: A < B + C e B < A + C e C < A + B
+ * Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B = C)
+ * Triângulo escaleno: possui todos os lados diferentes (A<>B e B <> C)
+ * Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
  */
 
 const prompt = require('prompt-sync')();
@@ -24,7 +31,7 @@ function solicitaLadosTriangulo() {
 function classificaTriangulo(lados) {
     // Ordena os lados em ordem crescente
     const [lado1, lado2, lado3] = lados.sort((a, b) => a - b);
-    if (lado1 + lado2 > lado3) {
+    if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2) {
         if (lado1 === lado2 && lado2 === lado3) {
             console.log('Triângulo equilátero.');
         } else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
